516a856345b09dc636da24d24b00efac56f38adf
Harry-Andrews <113364246+Harry-Andrews@users.noreply.github.com>
2022-10-05 19:45:46 +0100

    Update main.py
    Added input functionality to the program

--- main.py
+++ main.py
@@ -7,7 +7,39 @@ with open("valid_guesses.pkl", "rb") as f:
 
 with open("answer_list.pkl", "rb") as f:
     answer_list = pickle.load(f)
+#### This section takes the input guesses that you would like to score
+valid_input = False
 
+while not valid_input:
+    try:
+        number_of_words = int(input("How many starting Wordle guesses would you like to score? "))
+    except:
+        print("Invalid Input! Please try again.")
+    else:
+        if number_of_words > 6:
+            print("That's too many words for a wordle game")
+        elif number_of_words < 1:
+            print("That's not a positive number of words!")
+        else:
+            valid_input = True
+input_words = []
+i = 0
+while number_of_words > 0:
+    orders = ["first", "second", "third", "forth", "fifth", "sixth"]
+    input_word = input(f"What is your {orders[i]} word? ")
+    if len(input_word) != 5:
+        print("That is not a valid input please try again: ")
+        continue
+    else:
+        input_words.append(input_word)
+        number_of_words -= 1
+        i += 1
+lower_case_words = []
+for word in input_words:
+    lower_case_words.append(word.lower())
+input_words = lower_case_words.copy()    
+#### End of input section
+    
 # guess_combo = ["quick", "brown", "foxxy"]
 # The original intention of this project was to find the best 3 words to guess every time, this has failed as
 # There are over 280 billion different ways of choosing each combination and it takes about 80 seconds for me to check
@@ -28,8 +60,7 @@ best_word_score = 2000
 # for valid_guess in valid_guesses:
 #     print(f"Now working on {valid_guess}")
 #     max_size_of_worst_case = 0
-
-guess_combinations = [["SALET", "DUCKY"] ["SALET", "PRION"]]
+guess_combinations = [input_words]
 
 now = time.perf_counter()
 
